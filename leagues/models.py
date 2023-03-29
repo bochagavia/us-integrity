80e932ddaf496a392d4bd9f31e6aa62a224fd5d5
Baltazar <bochagavia@Baltazars-MacBook-Air.local>
2023-03-29 10:04:52 -0500

    Added plural names for models

--- leagues/models.py
+++ leagues/models.py
@@ -5,6 +5,9 @@ class Leagues(models.Model):
     abbr = models.CharField(max_length=10)
     name = models.CharField(max_length=200)
 
+    class Meta:  
+        verbose_name_plural = 'Leagues'
+
     def __str__(self):
          return self.abbr
 
@@ -13,6 +16,9 @@ class Teams(models.Model):
     league = models.ForeignKey(Leagues, on_delete=models.SET_NULL, null=True)
     abbr = models.CharField(max_length=10)
     name = models.CharField(max_length=200)
+
+    class Meta:  
+        verbose_name_plural = 'Teams'
 
     def __str__(self):
          return self.abbr
